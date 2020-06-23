7510b318f0f617e28cc9f522d4d76eaf7fa0e7af
Daniel Saidi <daniel.saidi@gmail.com>
2020-06-24 00:05:33 +0200

    Add stuff to the TV app

--- BuildSwiftUIAppsForTvOS/BuildSwiftUIAppsForTvOS/HomeScreen.swift
+++ BuildSwiftUIAppsForTvOS/BuildSwiftUIAppsForTvOS/HomeScreen.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct HomeScreen: View {
+    
+    @Environment(\.resetFocus) var resetFocus
+    @Namespace private var namespace
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button(action: {}) {
+                Text("Custom")
+                    .padding()
+                    .background(Color.green)
+            }.background(Color.green)
+            .buttonStyle(MyButtonStyle())
+            .prefersDefaultFocus(false, in: namespace)
+            
+            Button(action: { /*resetFocus(in: namespace)*/ }) {
+                ButtonView()
+            }
+            .prefersDefaultFocus(true, in: namespace)
+            .buttonStyle(CardButtonStyle())
+            .contextMenu {
+                Text("Menu Item 1")
+                Text("Menu Item 2")
+                Text("Menu Item 3")
+            }.onPlayPauseCommand { print("Play/pause") }
+        }.focusScope(namespace)
+    }
+}
+
+struct ButtonView: View {
+    
+    @Environment(\.isFocused) var isFocused: Bool
+    
+    var body: some View {
+        ZStack {
+            Color.red.frame(width: 300, height: 200, alignment: .center).cornerRadius(20)
+            Text(isFocused ? "DO ME!" : "FOCUS ME!")
+        }
+    }
+}
+
+struct MyButtonStyle: ButtonStyle {
+        
+    func makeBody(configuration: Configuration) -> some View {
+        let pressScale: CGFloat = configuration.isPressed ? 1.2 : 1.0
+        return configuration.label
+            .scaleEffect(x: pressScale, y: pressScale, anchor: .center)
     }
 }
 
